e51fab481d2462879ce512a6ead3a6af6e6c27e8
Carlos Eduardo Castiglione <kadu2095cf@gmail.com>
2020-05-13 21:26:37 -0300

    Criação do pacote de cliente e encapsulamento e DB de clientes

--- src/java/fatec/cliente/DBCliente.java
+++ src/java/fatec/cliente/DBCliente.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package fatec.cliente;
-
+import java.util.ArrayList;
 /**
  *
  * @author luccas
+ * modificado por carlos
  */
 public class DBCliente {
-    
+    private static ArrayList<Cliente> clientes;
+    public static ArrayList<Cliente> getClientes(){
+        if (clientes == null){
+            clientes = new ArrayList<>();
+            Cliente admin = new Cliente();
+            admin.setAttributes("Carlos", "12312312332", "123213211", "cadu@gmail.com", "34253212", "rua araxa");
+            clientes.add(admin);
+            Cliente cliente = new Cliente();
+            admin.setAttributes("Eduardo", "42312312302", "023013011", "edu@gmail.com", "04500312", "rua dois");
+            clientes.add(cliente);       
+        }
+        
+        return clientes;
+    }
 }
+
